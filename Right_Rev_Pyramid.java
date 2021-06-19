d3a982f62f1468730472cca00cf1e7b15f13f26c
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-19 11:14:32 +0530

    Add files via upload

---
+++ Right_Rev_Pyramid.java
@@ -0,0 +1,12 @@
+public class Right_Rev_Pyramid{
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++) // 5 rows - 1 2 3 4 5
+		{
+		for (int j=5; j >= i ;j--) // i times
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	} }
